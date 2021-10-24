f5c2566b35ed87195bc22ffb3c00d77153b672d3
sstahl0813 <sophie.stahl2001@gmail.com>
2021-10-24 00:12:37 -0400

    board done?

--- BattleBoard.java
+++ BattleBoard.java
@@ -5,6 +5,7 @@ import java.util.*;
 // miss = 3
 public class BattleBoard {
     private int[][] board;
+    private ArrayList<ArrayList<Integer>> ships = new ArrayList<ArrayList<Integer>>();
 
     public BattleBoard(){
         this.board = new int[8][8];
@@ -36,10 +37,10 @@ public class BattleBoard {
                         System.out.print("s ");
                         break;
                     case 2:
-                        System.out.print("o ");
+                        System.out.print("x ");
                         break;
                     case 3:
-                        System.out.print("x ");
+                        System.out.print("o ");
                         break;
                 }
             }
@@ -141,6 +142,273 @@ public class BattleBoard {
             System.out.println();
         }
     }
+
+      public void shipAdd(){
+        Scanner scan = new Scanner(System.in);
+        int[] shipLengths = new int[]{5,4,3,3,2};
+        for (int i=0; i <shipLengths.length; i++){
+            System.out.println("This is your board:");
+            printBoard();
+            System.out.println("Please enter where you want the head of the ship (length " + shipLengths[i] + ")");
+            String input = scan.nextLine();
+            String[] check = input.split(" ");
+            int[] coords = stringToCoord(check[0]);
+            boolean valid = checkValid(check[1], shipLengths[i], coords);
+            while(!valid){
+                System.out.println("Invalid Ship Placement");
+                System.out.println("Please enter where you want the head of the ship (length " + shipLengths[i] + ")");
+                input = scan.nextLine();
+                check = input.split(" ");
+                coords = stringToCoord(check[0]);
+                valid = checkValid(check[1], shipLengths[i], coords);
+            }
+            shipPlace(check[1], shipLengths[i], coords);
+        }
+    }
+
+    public boolean checkValid(String orientation, int length, int[] coords){
+        if (coords[0] == -1 || coords[1] == -1){
+            return false;
+        }
+        if (orientation.contains("v")){
+            if (coords[1]+length>8 ){
+                return false;
+            }
+            for (int i = coords[1]; i<(length + coords[1]); i++){
+                if (getValue(i,coords[0]) == 1){
+                    return false;
+                }else{
+                    if (!checkAround(i, coords[0], 1)){
+                        return false;
+                    }
+                }
+            }
+        }else{
+            if (coords[0]+length>8){
+                return false;
+            }
+            for (int i = coords[0]; i<(length + coords[0]); i++){
+                if (getValue(coords[1],i) == 1){
+                    return false;
+                }else{
+                    if (!checkAround(coords[1], i, 1)){
+                        return false;
+                    }
+                 }
+            }
+        }
+        return true;
+    }
+
+
+    public void shipRandom(){
+        int[] shipLengths = new int[]{5,4,3,3,2};
+        Random rand = new Random();
+        for (int i =0; i<shipLengths.length; i++){
+            int row = rand.nextInt((7 - 0) + 1) + 0;
+            int column = rand.nextInt((7 - 0) + 1) + 0;
+            int ori = rand.nextInt((1 - 0) + 1) + 0;
+            int[] coords = new int[]{row, column};
+            String orientation = "";
+            if (ori==1){
+                orientation = "v";
+            }else{
+                orientation = "h";
+            }
+            boolean valid = checkValid(orientation, shipLengths[i], coords);
+            while(!valid){;
+                row = rand.nextInt((7 - 0) + 1) + 0;
+                column = rand.nextInt((7 - 0) + 1) + 0;
+                ori = rand.nextInt((1 - 0) + 1) + 0;
+                coords = new int[]{row, column};
+                if (ori==1){
+                    orientation = "v";
+                }else{
+                    orientation = "h";
+                }
+                valid = checkValid(orientation, shipLengths[i], coords);
+            }
+            shipPlace(orientation, shipLengths[i], coords);
+        }
+    }
+
+    public boolean checkAround(int r, int c, int val){
+        if (r==7 || r==0 || c==7 || c==0){
+            if (r==7){
+                if (c==7){
+                    if (getValue(r-1,c-1) == val || getValue(r,c-1) == val || getValue(r-1,c) == val){
+                        return false;
+                    }
+                }else if (c==0){
+                    if (getValue(r-1,c) == val || getValue(r-1,c+1) == val || getValue(r,c+1) == val){
+                        return false;
+                    }
+                }else{
+                    if (getValue(r-1,c-1) == val || getValue(r,c-1) == val || getValue(r-1,c) == val  || getValue(r-1,c+1) == val || getValue(r,c+1) == val){
+                        return false;
+                    }
+                }
+            }else if(r==0){
+                if (c==7){
+                    if (getValue(r,c-1) == val || getValue(r+1,c-1) == val || getValue(r+1,c) == val){
+                        return false;
+                    }
+                }else if (c==0){
+                    if (getValue(r,c+1) == val || getValue(r+1,c+1) == val || getValue(r+1,c) == val){
+                        return false;
+                    }
+                }else{
+                    if (getValue(r,c-1) == val || getValue(r+1,c-1) == val || getValue(r+1,c) == val  || getValue(r+1,c+1) == val || getValue(r,c+1) == val){
+                        return false;
+                    }
+                }
+            }else if (c==7){
+                if (getValue(r-1,c-1) == val|| getValue(r-1,c) == val|| getValue(r,c-1) == val|| getValue(r+1,c-1) == val|| getValue(r+1,c) == val){
+                    return false;
+                }
+            }else{
+                if (getValue(r-1,c) == val|| getValue(r-1,c+1) == val|| getValue(r,c+1) == val|| getValue(r+1,c+1) == val|| getValue(r+1,c) == val){
+                    return false;
+                }
+            }
+        }else{
+            if (getValue(r-1,c-1) == val || getValue(r-1,c) == val || getValue(r-1,c+1) == val|| getValue(r,c-1) == val|| getValue(r,c+1) == val|| getValue(r+1,c-1) == val|| getValue(r+1,c) == val|| getValue(r+1,c+1) == val){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean setAround(int r, int c, int val){
+        if (r==7 || r==0 || c==7 || c==0){
+            if (r==7){
+                if (c==7){
+                    setValue(r-1,c-1, val);
+                    setValue(r,c-1,val);
+                    setValue(r-1,c,val);
+                }else if (c==0){
+                    setValue(r-1,c, val);
+                    setValue(r-1,c+1, val);
+                    setValue(r,c+1,val);
+                }else{
+                    setValue(r-1,c-1,val);
+                    setValue(r,c-1,val);
+                    setValue(r-1,c,val); 
+                    setValue(r-1,c+1,val);
+                    setValue(r,c+1,val);
+                }
+            }else if(r==0){
+                if (c==7){
+                    setValue(r,c-1,val);
+                    setValue(r+1,c-1,val);
+                    setValue(r+1,c,val);
+                }else if (c==0){
+                    setValue(r,c+1,val);
+                    setValue(r+1,c+1,val);
+                    setValue(r+1,c,val);
+                    
+                }else{
+                    setValue(r,c-1, val);
+                    setValue(r+1,c-1, val);
+                    setValue(r+1,c,val);
+                    setValue(r+1,c+1, val);
+                    setValue(r,c+1, val);
+                }
+            }else if (c==7){
+                setValue(r-1,c-1,val);
+                setValue(r-1,c,val);
+                setValue(r,c-1,val);
+                setValue(r+1,c-1,val);
+                setValue(r+1,c,val);
+            }else{
+                setValue(r-1,c,val);
+                setValue(r-1,c+1,val);
+                setValue(r,c+1,val);
+                setValue(r+1,c+1,val);
+                setValue(r+1,c,val);
+            }
+        }else{
+            setValue(r-1,c-1,val);
+            setValue(r-1,c,val);
+            setValue(r-1,c+1,val);
+            setValue(r,c-1,val);
+            setValue(r,c+1,val);
+            setValue(r+1,c-1,val);
+            setValue(r+1,c,val);
+            setValue(r+1,c+1,val);
+        }
+        return true;
+    }
+
+    public void shipPlace(String info, int length, int[] coords){
+        ArrayList<Integer> current = new ArrayList<Integer>();
+        if (info.contains("v")){
+            for (int i = coords[1]; i<(length + coords[1]); i++){
+                setValue(i, coords[0], 1);
+                current.add(i);
+                current.add(coords[0]);
+            }
+        }else{
+            for (int i = coords[0]; i<(length + coords[0]); i++){
+                setValue(coords[1],i, 1);
+                current.add(coords[1]);
+                current.add(i);
+            }
+        }
+        ships.add(current);
+    }
+
+    public void updateBoard(int[] coords){
+        int spot = getValue(coords[0], coords[1]);
+        System.out.println(spot);
+        if (spot == 1){
+            setValue(coords[0], coords[1], 2);
+        }else{
+            setValue(coords[0], coords[1], 3);
+        }
+
+        if (getValue(coords[0], coords[1])==2){
+            System.out.println("this works");
+            if (checkAround(coords[0], coords[1], 1)){
+                System.out.println("we got here");
+                ArrayList<Integer> shipGone = new ArrayList<Integer>();
+                for(ArrayList<Integer> curr : ships){
+                    for (int i = 0; i < curr.size(); i=i+2){
+                        if (curr.get(i)==coords[0] && curr.get(i+1)==coords[1]){
+                            System.out.println("found ship");
+                            shipGone = curr;
+                            break;
+                        }
+                    }
+                    if (!shipGone.isEmpty()){
+                        break;
+                    }
+                }
+                boolean allGone = true;
+                for (int i=0; i<shipGone.size(); i=i+2){
+                    System.out.println(i);
+                    System.out.println(i+1);
+                    if (getValue(shipGone.get(i),shipGone.get(i+1))!=2){
+                        System.out.println("still there!");
+                        System.out.println(shipGone);
+                        allGone=false;
+                        break;
+                    }
+                }
+                if (allGone){
+                    System.out.println("all gone");
+                    for (int i=0; i<shipGone.size(); i=i+2){
+                        setAround(shipGone.get(i), shipGone.get(i+1), 3);
+                    }
+                    for (int i=0; i<shipGone.size(); i=i+2){
+                        setValue(shipGone.get(i), shipGone.get(i+1), 2);
+                    }
+                }
+            }
+        }
+    }
+
+
     // public static void main(String args[]){
     //     BattleBoard test = new BattleBoard();
     //     BattleBoard opp = new BattleBoard();
--- Game.java
+++ Game.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+import java.util.*;
 public class Game {
 
     public Game(){
@@ -9,77 +9,22 @@ public class Game {
     public void shipSetUp(){
         Scanner scan = new Scanner(System.in);
         BattleBoard board = new BattleBoard();
-        shipAdd(board);
+        BattleBoard board2 = new BattleBoard();
+        // board.shipAdd();
+        board.shipRandom();
+        board2.shipRandom();
         board.printBoard();
+        System.out.println();
+        board.printBothBoards(board2);
     }
 
-    public void shipAdd(BattleBoard board){
-        Scanner scan = new Scanner(System.in);
-        int[] shipLengths = new int[]{5,4,3,3,2};
-        for (int i=0; i <shipLengths.length; i++){
-            System.out.println("This is your board:");
-            board.printBoard();
-            System.out.println("Please enter where you want the head of the ship (length " + shipLengths[i] + ")");
-            String input = scan.nextLine();
-            String[] check = input.split(" ");
-            boolean valid = checkValid(check, shipLengths[i], board);
-            while(!valid){
-                System.out.println("Invalid Ship Placement");
-                System.out.println("Please enter where you want the head of the ship (length " + shipLengths[i] + ")");
-                input = scan.nextLine();
-                check = input.split(" ");
-                valid = checkValid(check, shipLengths[i], board);
-            }
-            board = shipPlace(board, check, shipLengths[i]);
-        }
-    }
 
-    public BattleBoard shipPlace(BattleBoard board, String[] info, int length){
-        int[] coords = board.stringToCoord(info[0]);
-        if (info[1].contains("v")){
-            for (int i = coords[1]; i<(length + coords[1]); i++){
-                board.setValue(i, coords[0], 1);
-            }
-        }else{
-            for (int i = coords[0]; i<(length + coords[0]); i++){
-                board.setValue(coords[1],i, 1);
-            }
-        }
-        return board;
-    }
-
-    public boolean checkValid(String[] input, int length, BattleBoard board){
-        String orientation = input[1];
-        int[] coords = board.stringToCoord(input[0]);
-        System.out.println("row = " + coords[1]);
-        System.out.println("column = " + coords[0]);
-        System.out.println("ord " + orientation);
-        if (orientation.contains("v")){
-            if (coords[1]+length>8 ){
-                return false;
-            }
-            for (int i = coords[1]; i<(length + coords[1]); i++){
-                if (board.getValue(i,coords[0]) == 1){
-                    return false;
-                }
-            }
-        }else{
-            if (coords[0]+length>8){
-                return false;
-            }
-            for (int i = coords[0]; i<(length + coords[0]); i++){
-                if (board.getValue(coords[1],i) == 1){
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
     public static void main(String args[]){
         // BattleBoard test = new BattleBoard();
         // BattleBoard opp = new BattleBoard();
         // test.printBothBoards(opp);
         Game test = new Game();
+
         test.shipSetUp();
     }
 }
